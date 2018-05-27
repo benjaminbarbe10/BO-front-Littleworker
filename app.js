b5ac2a5cc00e1f53ed7908c11924d7adee1dd95a
benjaminbarbe10 <benjamin.barbe@viacesi.fr>
2018-05-28 00:07:09 +0200

    Handling POST requests using Joi for validation

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+//Pascal naming convention joi= class so J
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+
+//return piece of middleware
+app.use(express.json());
 
 const adverts = [
     { id: 1, name: 'advert1' },
@@ -14,6 +19,30 @@ app.get('/', (req, res) => {
 app.get('/api/adverts', (req, res) => {
     res.send([1, 2, 3]);
 });
+
+//Manually asign ID because not Working with a Database
+app.post('/api/adverts', (req,res) => {
+    const schema ={
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.validate(req.body, schema);
+
+    if(result.error) {
+        // 400 Bad Request
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    const advert = {
+        id: adverts.length +1,
+        name: req.body.name,
+    }
+    adverts.push(advert);
+    res.send(advert);
+});
+
+
 
 app.get('/api/adverts/:id', (req, res) => {
     const advert = adverts.find(c => c.id === parseInt(req.params.id));
