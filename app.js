6c3ae86db03b59769fc419faf5dfd93850cb1b7b
benjaminbarbe10 <benjamin.barbe@viacesi.fr>
2018-05-30 21:07:12 +0200

    Server Port Updated

--- app.js
+++ app.js
@@ -24,5 +24,5 @@ app.use('/', home)
 // ─── SERVER ─────────────────────────────────────────────────────────────────────
 //
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
